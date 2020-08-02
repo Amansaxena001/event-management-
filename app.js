f954f009cd3b27160fa332a129b2929caa7344ca
SAXENA <saxenaaman800@gmail.com>
2020-08-02 13:44:26 +0530

    First

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Event = require('./models/event');
+var Comment = require('./models/comment');
+var methodOverride = require('method-override');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
+
+//Requiring the route files
+var commentRoutes = require('./routes/comments');
+var eventRoutes = require('./routes/events');
+var indexRoutes = require('./routes/index');
+
+// seedDB(); // Seed the db
+// mongoose.connect('mongodb://localhost/eventdb');
+const db='mongodb://aman:amansaxena@cluster0-shard-00-00-5cozt.mongodb.net:27017,cluster0-shard-00-01-5cozt.mongodb.net:27017,cluster0-shard-00-02-5cozt.mongodb.net:27017/event?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+mongoose.connect(db,{useNewUrlParser:true})
+.then(()=>console.log('MongoDB ONLINE'))
+.catch(err=>console.log(err))
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+
+//Passport config
+app.use(require('express-session')({
+  secret: 'Passcode!!!',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Middleware for passing currentUser to all templates
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+//Use the requrired route files
+app.use(indexRoutes);
+app.use(commentRoutes);
+app.use(eventRoutes);
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+  console.log("Server Started...");
+});
---
+++ routes/comments.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var Event = require('../models/event');
+var Comment = require('../models/comment');
+
+//Add new comment form
+router.get('/events/:id/comments/new', isLoggedIn, function(req, res){
+  Event.findById(req.params.id, function(err, event){
+    res.render('comments/new', {event: event});
+  });
+});
+
+//Create comment route
+router.post('/events/:id/comments', isLoggedIn, function(req, res){
+  Event.findById(req.params.id, function(err, event){
+    if(err){
+      console.log(err);
+        res.redirect('events');
+    }
+    else{
+      Comment.create(req.body.comment, function(err, comment){
+        if(err){
+          console.log(err);
+        }
+        else{
+          //add username and id to comment
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          //save comment
+          comment.save();
+          event.comments.push(comment);
+          event.save();
+          res.redirect('/events/' + event._id);
+        }
+      });
+    }
+  });
+});
+
+//Middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  else{
+    res.redirect("/login");
+  }
+}
+
+module.exports = router;
