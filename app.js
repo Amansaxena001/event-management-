b181cfc63b05105e3e40f85c5d1f096540e73b08
Amansaxena001 <49619101+Amansaxena001@users.noreply.github.com>
2020-09-14 22:53:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var indexRoutes = require('./routes/index');
 
 // seedDB(); // Seed the db
 // mongoose.connect('mongodb://localhost/eventdb');
-const db='mongodb://aman:amansaxena@cluster0-shard-00-00-5cozt.mongodb.net:27017,cluster0-shard-00-01-5cozt.mongodb.net:27017,cluster0-shard-00-02-5cozt.mongodb.net:27017/event?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+const db='YOUR MONGO URI'
 mongoose.connect(db,{useNewUrlParser:true})
 .then(()=>console.log('MongoDB ONLINE'))
 .catch(err=>console.log(err))
